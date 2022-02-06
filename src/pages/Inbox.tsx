f8e22bdd91adf0a43f88f6fe4702e9fd0ee10cba
Panaite Cristian-Florin <panaite.cristian12@gmail.com>
2022-02-06 20:21:33 +0200

    doamne

--- src/pages/Inbox.tsx
+++ src/pages/Inbox.tsx
@@ -1,8 +1,50 @@
 import React from 'react';
 
 function Inbox() {
-  return <div>
-      <h1>Inbox</h1>
+  return <div className='inbox-page'>
+      <div className='inbox-container'>
+        <div className='email'>
+          <div className='email-img-text'>
+          <img src='./logo512.png' alt="logo"/>
+
+          <div className='email-text'>
+          <h3>Irimia Mihai</h3>
+          <h4>irimiamihai01@yahoo.com</h4>
+          </div>
+
+          </div>
+          <p>Hai salut ! Intram la acea intalnire?</p>
+        </div>
+        <div className='email'>
+          <div className='email-img-text'>
+          <img src='./logo512.png' alt="logo"/>
+
+          <div className='email-text'>
+          <h3>Irimia Mihai</h3>
+          <h4>irimiamihai01@yahoo.com</h4>
+          </div>
+
+          </div>
+          <p>Hai salut ! Intram la acea intalnire?</p>
+        </div>
+        <div className='email'>
+          <div className='email-img-text'>
+          <img src='./logo512.png' alt="logo"/>
+
+          <div className='email-text'>
+          <h3>Irimia Mihai</h3>
+          <h4>irimiamihai01@yahoo.com</h4>
+          </div>
+
+          </div>
+          <p>Hai salut ! Intram la acea intalnire?</p>
+        </div>
+      </div>
+      <div className='something'>
+          <p>
+            i don't care bro           
+          </p>
+      </div>
   </div>;
 }
 
